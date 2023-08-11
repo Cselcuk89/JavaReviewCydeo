21278b637025d9aee451c581cb0fd5ec59b1a321
Cselcuk89 <cagriselcuk89@gmail.com>
2023-08-11 13:33:20 -0500

    method reference intro

---
+++ lambda-expressions/src/main/java/org/Selcuk/doubleColonOperator/Calculator.java
@@ -0,0 +1,10 @@
+package org.Selcuk.doubleColonOperator;
+
+public class Calculator {
+    public static void findSum(int x,int y){
+        System.out.println("sum is : " + (x+y));
+    }
+    public void findMultiplication(int x,int y){
+        System.out.println("Multiplication is : " + (x*y));
+    }
+}
---
+++ lambda-expressions/src/main/java/org/Selcuk/doubleColonOperator/Demo.java
@@ -0,0 +1,38 @@
+package org.Selcuk.doubleColonOperator;
+
+import java.util.function.BiFunction;
+
+public class Demo {
+    public static void main(String[] args) {
+        //reference to a static method using lambda
+        Calculate s1 = (x,y) -> Calculator.findSum(x,y);
+        s1.calculate(10,20);
+
+        //using double colon operator
+        Calculate s2 =Calculator::findSum;
+        s2.calculate(100,45);
+
+        //reference to instance method using lambda
+        Calculate m1 = (x,y) -> new Calculator().findMultiplication(x,y);
+        m1.calculate(24,4);
+
+        //using double colon for instance method
+        Calculator obj = new Calculator();
+        Calculate m2 = obj::findMultiplication;
+        m2.calculate(25,10);
+        //or
+        Calculate m3 = new Calculator()::findMultiplication;
+        m3.calculate(25,5);
+
+        //using BiFunction interface with both lambda and double colon operator
+        //with lambda
+        BiFunction<String,Integer,String> fn = (str,i) -> str.substring(i);
+        System.out.println(fn.apply("Developer",6));
+        //with double colon operator
+        BiFunction<String,Integer,String> fn2 = String::substring;
+        System.out.println(fn2.apply("Developer",6));
+
+
+
+    }
+}
